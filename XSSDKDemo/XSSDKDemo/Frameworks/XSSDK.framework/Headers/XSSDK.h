b4a7746cbd486f51243f4836138b0ce7410eb51e
熙文 张 <Seven@xiwendeMacBook-Pro.local>
2017-04-27 15:04:51 +0800

    新增项目
    新增项目

---
+++ XSSDKDemo/XSSDKDemo/Frameworks/XSSDK.framework/Headers/XSSDK.h
@@ -0,0 +1,96 @@
+//
+//  XSSDK.h
+//  XSSDK
+//
+//  Created by 熙文 张 on 17/4/16.
+//  Copyright © 2017年 熙文 张. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "XSUser.h"
+#import "XSOrder.h"
+
+//! Project version number for XSSDK.
+FOUNDATION_EXPORT double XSSDKVersionNumber;
+
+//! Project version string for XSSDK.
+FOUNDATION_EXPORT const unsigned char XSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XSSDK/PublicHeader.h>
+
+
+typedef void (^loginSuccessBlock)(XSUser *user);
+typedef void (^logOutBlock)();
+
+
+@interface XSSDK : NSObject
+
+/**
+ *  游戏编号
+ */
+@property (nonatomic, readonly, assign) int gameId;
+
+/**
+ *  游戏渠道
+ */
+@property (nonatomic, readonly, assign) int channelId;
+
+/**
+ *  游戏密钥
+ */
+@property (nonatomic, readonly, strong) NSString *secretKey;
+
+/**
+ *  登陆成功后当前用户信息
+ */
+@property (nonatomic, strong) XSUser *currUser;
+
+@property (nonatomic, copy) loginSuccessBlock loginSuccessBlock;
+@property (nonatomic, copy) logOutBlock logOutBlock;
+
+/**
+ *  获取XSSDK单例
+ *
+ *  @return XSSDK单例对象
+ */
++ (XSSDK *)sharedXSSDK;
+
+
+
+/**
+ *  初始化SDK
+ *
+ *  @param gameId    游戏编号
+ *  @param channelId 游戏渠道
+ *  @param secretKey 游戏密钥
+ */
+- (void)xsInitWithSDKParameters:(int)gameId channelId:(int)channelId secretKey:(NSString *)secretKey;
+
+
+/**
+ *   用户登陆
+ *
+ *  @param successBlock 登陆成功回调
+ */
+- (void)xsLogin:(loginSuccessBlock)successBlock;
+
+
+/**
+ *  支付
+ *
+ *  @param order    订单信息
+ */
+- (void)xsPay:(XSOrder *)order;
+
+
+/**
+ *  注销用户登陆接口
+ */
+- (void)xsLogOut;
+
+/**
+ *  注销事件回调
+ */
+- (void)setLogOutBlock:(logOutBlock)logOutBlock;
+
+@end
---
+++ XSSDKDemo/XSSDKDemo/ViewController.m
@@ -0,0 +1,106 @@
+//
+//  ViewController.m
+//  XSSDKDemo
+//
+//  Created by 熙文 张 on 17/4/26.
+//  Copyright © 2017年 熙文 张. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <XSSDK/XSSDK.h>
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIImage *buttonImage = [UIImage imageNamed:@"XS_SDK_Button_Normal"];
+    UIImage *buttonImageHighlighted = [UIImage imageNamed:@"XS_SDK_Button_Highlighted"];
+    UIButton *loginButton = [UIButton new];
+    [loginButton setFrame:CGRectMake(140, 140, 100, 36)];
+    [loginButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [loginButton setBackgroundImage:buttonImage forState:UIControlStateNormal];
+    [loginButton setBackgroundImage:buttonImageHighlighted forState:UIControlStateHighlighted];
+    [loginButton setTitle:@"登陆" forState:UIControlStateNormal];
+    [loginButton addTarget:self action:@selector(loginButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:loginButton];
+    
+    
+    UIButton *payButton = [UIButton new];
+    [payButton setFrame:CGRectMake(140, 200, 100, 36)];
+    [payButton setTitle:@"买个皮肤" forState:UIControlStateNormal];
+    [payButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [payButton setBackgroundImage:buttonImage forState:UIControlStateNormal];
+    [payButton addTarget:self action:@selector(payButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:payButton];
+    
+    UIButton *logOutButton = [UIButton new];
+    [logOutButton setFrame:CGRectMake(140, 260, 100, 36)];
+    [logOutButton setTitle:@"注销" forState:UIControlStateNormal];
+    [logOutButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [logOutButton setBackgroundImage:buttonImage forState:UIControlStateNormal];
+    [logOutButton addTarget:self action:@selector(logOutButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:logOutButton];
+    
+    [[XSSDK sharedXSSDK] setLogOutBlock:^{
+        NSLog(@"注销事件的回调");
+    }];
+}
+
+
+#pragma mark --支付按钮事件--
+- (void)payButtonClick
+{
+    XSOrder *order = [XSOrder new];
+    [order setServerId:@"serverId8"];
+    [order setAmount:1];
+    [order setRoleId:@"465689"];
+    [order setRoleName:@"锐雯"];
+    [order setProductName:@"冠军之刃"];
+    [order setProductDescription:@"皮肤"];
+    [order setOrderId:[self getOrderStringByTime]];
+    [order setCustomInfo:@"断剑重铸之日 骑士归来之时"];
+    [order setProductId:@"com.xshd.SDKDemo.6"];
+    [[XSSDK sharedXSSDK] xsPay:order];
+}
+
+
+#pragma mark --登陆按钮事件--
+- (void)loginButtonClick
+{
+    [[XSSDK sharedXSSDK] xsLogin:^(XSUser *user) {
+        NSString *userId    = user.userId;
+        NSString *userName  = user.username;
+        NSString *token = user.token;
+        NSLog(@"userId    -- %@", userId);
+        NSLog(@"userName  -- %@", userName);
+        NSLog(@"token     -- %@", token);
+    }];
+}
+
+
+- (void)logOutButtonClick
+{
+    [[XSSDK sharedXSSDK] xsLogOut];
+}
+
+
+- (NSString *)getOrderStringByTime
+{
+    NSDate *date = [NSDate date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"YYYYMMddhhmmssSS"];
+    NSString *dateString = [dateFormatter stringFromDate:date];
+    NSLog(@"order:%@", dateString);
+    return dateString;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
